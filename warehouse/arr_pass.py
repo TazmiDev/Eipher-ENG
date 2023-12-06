f512c9344da60a3191480cc476e1719a34d08f85
Tazmi <152580586+TazmiDev@users.noreply.github.com>
2023-12-06 16:06:33 +0800

    Update arr_pass.py
    Fix bugs.

--- warehouse/arr_pass.py
+++ warehouse/arr_pass.py
@@ -2,13 +2,12 @@ import itertools
 
 
 def list_handle(my_str):
-    my_list = my_str.split()
+    my_list = input_str.split()
     unique_list = list(set(my_list))
-    permutations = list(itertools.permutations(unique_list))
     with open("output/passwords.txt", "w") as file:
-        for perm in permutations:
-            pwd = ''.join(str(item) for item in perm)
-            file.write(pwd + "\n")
-        print(
-            "\033[32mAfter the password is generated, please refer to the 'output/passwords.txt' file for "
-            "details.\033[0m")
+        for length in range(1, len(unique_list) + 1):
+            combinations = itertools.combinations(unique_list, length)
+            for comb in combinations:
+                pwd = ''.join(str(item) for item in comb)
+                file.write(pwd + "\n")
+    print("\033[32mAfter the password is generated, please refer to the 'output/passwords.txt' file for details.\033[0m")
